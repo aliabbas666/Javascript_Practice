8af6897069a565e934e1d478a53f96a4ec7f0759
aliabbas666 <aliabbas199555@gmail.com>
2021-06-26 19:27:49 +0500

    commit 1

---
+++ app.js
@@ -0,0 +1,11 @@
+// for(var i=1;i<=10;i++)
+// document.write("2 "+" * " +i +" ="+i*2+"<br>")
+
+// var n=prompt("Enter a number for table:")
+// for(var i=1;i<=10;i++)
+// document.write(n+" * " +i +" ="+i*n+"<br>")
+
+var n=prompt("Enter a number for table:")
+var m=prompt("Enter a limit for the table")
+for(var i=1;i<=m;i++)
+document.write(n+" * " +i +" ="+i*n+"<br>")
